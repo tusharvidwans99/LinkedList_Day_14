fb01a80e0f11b80d7a349d63d82b9d602175f046
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-06-06 06:06:32 +0530

    [Tushar Vidwans] Creating simple Linked List

---
+++ Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList_Day_14
+{
+    public class Node<T>
+    {
+        T data;
+        Node<T> next;
+
+        public Node(T data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+    }
+}
