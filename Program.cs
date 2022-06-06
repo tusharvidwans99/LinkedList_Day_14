b0f35d60b438ef92893f7bc4422bbf712cf160fd
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-06-06 06:02:25 +0530

    [Tushar Vidwans] Adding welcome message

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace LinkedList_Day_14
+{
+    /// <summary>
+    /// In this program we are going to do operations on LinkedList.
+    /// </summary>
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to this program in which we are doing operations on LinkedList");
+
+        }
+    }
+}
